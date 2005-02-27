09168e23daecf949396b94ea330f830db87e34be
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-02-27 16:01:18 +0000

    Last attempt..

---
+++ trunk/platform/Darwin.mk
@@ -0,0 +1,7 @@
+export MACOSX_DEPLOYMENT_TARGET=10.3
+unexport LD_PREBIND
+export GARLIBEXT=dylib
+export GARSHARED=-dynamic
+export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
+export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
+export MASKED := apps/base/gcc apps/base/globus apps/base/gpt apps/base/myproxy apps/base/openssl 
