35fece29c599841ad86268cef637d2128bd52940
nobody <nobody@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-05-24 04:56:37 +0000

    This commit was manufactured by cvs2svn to create tag 'v2-0-4'.

---
+++ tags/v2-0-4/platform/Darwin.mk
@@ -0,0 +1,8 @@
+TARGET_LIBNAME=lib
+export MACOSX_DEPLOYMENT_TARGET=10.3
+unexport LD_PREBIND
+export GARLIBEXT=dylib
+export GARSHARED=-dynamic
+export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
+export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
+export MASKED := apps/base/gcc apps/base/readline apps/base/ncurses apps/devel/dialog apps/base/openssl apps/base/bash apps/gui/glib apps/gui/Gtk-Perl apps/server/openldap
---
+++ tags/v2-0-4/provides.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+PREFIX=$1; shift
+COOKIEDIR=$1; shift
+
+ProvidesStart()
+{
+  mkdir -p $COOKIEDIR
+  rm -rf $COOKIEDIR/.provides  $COOKIEDIR/provides*   
+  touch $COOKIEDIR/provides
+  sleep 2   
+}
+
+
+ProvidesStop()
+{
+  if [ -f files/provides ]
+  then
+     cp files/provides $COOKIEDIR
+  else
+    find $PREFIX -cnewer $COOKIEDIR/provides  -a \( -type f -o -type l \) > $COOKIEDIR/provides.all
+
+    grep /.packlist $COOKIEDIR/provides.all > $COOKIEDIR/provides.perl; 
+
+    if [ -s $COOKIEDIR/provides.perl ] 
+    then 
+      list=`cat $COOKIEDIR/provides.perl | grep -v 'perllocal.pod$'` 
+      for f in $list
+      do
+        if [ ! -z $f ]
+        then 
+          if [ `grep -c -e type=file -e type=link $f` -gt 0 ] 
+          then 
+            grep -e type=file -e type=link $f | awk '{print $1}' >> $COOKIEDIR/provides.all
+          else
+            cat $f >> $COOKIEDIR/provides.all
+          fi
+        fi
+      done
+    fi
+    sort -u $COOKIEDIR/provides.all | grep -v /.packlist > $COOKIEDIR/provides 
+  fi
+}
+
+
+case $1 in 
+  start|Start) 
+       ProvidesStart
+       exit
+       ;;
+  stop|Stop) 
+       ProvidesStop
+       exit
+       ;;
+  *) 
+       echo "Usage: provides.sh <PREFIX> <COOKIEDIR> <start|stop>"
+       exit 1
+       ;;
+esac
