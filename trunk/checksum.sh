dae2054acd5477163db4384ad6684b1c95876331
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-06-21 15:19:08 +0000

    Fixing checksum calculation on Mac

--- trunk/checksum.sh
+++ trunk/checksum.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
+case `uname` in
+    Darwin)
+      MD5="md5sum -v"
+      ;;
+    *)       
+      MD5="md5sum"
+      ;;
+esac
+
 ChecksumPrint()
 {
-  if [ "$BININSTALL" != "" ]
-  then
-    tmpdir=`mktemp -d /tmp/checksum.XXXXXX`
-    dir=`dirname $1`
-    mkdir -p $tmpdir/$dir || exit 1
-    tar jxvf $1 -C $tmpdir/$dir > $tmpdir/list
-    cd $tmpdir/$dir
-    grep -v -e '.*/lib.*\.a$' ../list | awk '{printf("[ -f %s ] && md5sum %s\n",$1,$1)}' | sh | awk '{print $1} END{print NR}' | sort > `basename $1`
-    cd $tmpdir
-    md5sum $1
-    rm -rf $tmpdir
-  else
-    md5sum $*
-  fi
+  $MD5 $*
 }
 
 CheckFile()
@@ -24,14 +20,7 @@ CheckFile()
    then
      exit 1
    fi
-   sum=`grep $1 $checksums | awk '{print $1}'` || exit 1
-   if [ "$BININSTALL" != "" ]
-   then 
-     mysum=`ChecksumPrint $1 | awk '{print $1}'`
-     [ "$sum" != "$mysum" ] && exit 1 
-   else
-     env LC_ALL="C" LANG="C" md5sum -c $checksums 2>&1 | grep "$1:[ ]\+OK" || exit 1 
-   fi
+   env LC_ALL="C" LANG="C" $MD5 -c $checksums 2>&1 | grep "$1.*OK" || exit 1 
    exit 0
 }
 
@@ -51,11 +40,9 @@ case $1 in
   --help)
        echo "Usage: checksum.sh print <file_name>"
        echo "                   check <checksum file> <file>"
-       echo "                   package <file list> <file>"
        exit 0
        ;;
        *)
-       md5sum $*
-       exit $?
+       exit 2
        ;; 
 esac
