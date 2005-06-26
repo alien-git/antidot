566d78c0c9185498d500b6bd787d9a266a8ef3dd
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-06-26 22:52:25 +0000

    Added to repository

---
+++ platform.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+mode=$1; shift
+dir=`dirname $0`
+
+case `$dir/config.guess` in
+    i*86-*-linux-gnu)
+      platform=i686-pc-linux-gnu
+      autoplatform=x86
+      javaplatform=linux-i586
+     ;;
+    x86_64-*-linux-gnu)
+      platform=i686-pc-linux-gnu
+      autoplatform=x86_64
+      javaplatform=linux-x86_64
+      ;;
+    powerpc-apple-darwin7.*)
+      platform=powerpc-apple-darwin7.7.0
+      javaplatform=linux-ppc
+      ;;
+    ia64-*-linux-gnu)
+      platform=ia64-unknown-linux-gnu
+      autoplatform=ia64
+      javaplatform=linux-i64
+      ;;
+     *)
+      echo "Unknown or unsupported platform: $platform"
+      exit 1
+      ;;
+esac
+
+case $mode in
+    platform)
+       echo $platform
+       ;;
+    autoplatform)
+       echo $autoplatform
+       ;;
+    javaplatform)
+       echo $javaplatform
+       ;;
+     *)
+       echo "Unknown mode: $mode"
+       exit 1
+esac  
