7f442e7abad6e542a930cd01ccb60418c969fa72
nobody <nobody@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-07-08 13:23:41 +0000

    This commit was manufactured by cvs2svn to create tag 'v2-1-1'.

---
+++ tags/v2-1-1/relocate.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+prefix=$1; shift 1
+app=$1; shift 1
+
+build_prefix=/opt/alien
+if [ "$prefix" != "$build_prefix" ]
+then
+  case $app in 
+   */apps/base/perl)
+     config=`find $prefix/lib/perl5 -name Config.pm -exec grep -l "This file was created by configpm" {} \;`
+     perl -pi -e "s%$build_prefix/%$prefix/%sg; s%$build_prefix'%$prefix'%sg; s%$build_prefix %$prefix %sg " $config
+     config=`find $prefix/lib/perl5 -path "*/CORE/config.h"`
+     perl -pi -e "s%$build_prefix/%$prefix/%sg; s%$build_prefix'%$prefix'%sg; s%$build_prefix %$prefix %sg; s%\"$build_prefix\"%\"$prefix\"%sg; " $config
+     for file in `find $prefix/lib/perl5 -name .packlist`
+     do
+          perl -pi -e "s%$build_prefix/%$prefix/%g" $file
+     done
+     ;;
+     *)
+      ;;
+  esac 
+fi
