b5a4fff8cb8f69092de44bc4d90711596b904c1d
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-09-18 17:53:19 +0000

    Trying to unmask readline

--- platform/Darwin.mk
+++ platform/Darwin.mk
@@ -5,5 +5,5 @@ export GARLIBEXT=dylib
 export GARSHARED=-dynamic
 export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
 export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
-export MASKED := apps/gcc/gcc apps/base/readline apps/base/ncurses apps/devel/dialog apps/base/openssl apps/base/bash apps/gui/glib apps/gui/Gtk-Perl apps/server/openldap
+export MASKED := apps/gcc/gcc apps/base/ncurses apps/devel/dialog apps/base/openssl apps/base/bash apps/gui/glib apps/gui/Gtk-Perl apps/server/openldap
 
