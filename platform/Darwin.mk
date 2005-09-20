73637a8f8355f96a546f4d95cf5b1f10e5732580
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-09-20 08:43:56 +0000

    Masking non-relevant QT packages

--- platform/Darwin.mk
+++ platform/Darwin.mk
@@ -5,5 +5,5 @@ export GARLIBEXT=dylib
 export GARSHARED=-dynamic
 export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
 export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
-export MASKED := apps/gcc/gcc apps/base/bash apps/gui/glib apps/gui/Gtk-Perl 
+export MASKED := apps/gcc/gcc apps/base/bash apps/gui/glib apps/gui/Gtk-Perl apps/gui/qt-x11-opensource-src
 
