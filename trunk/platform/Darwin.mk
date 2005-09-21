68be45751d8b427518a0381ba6f44eabd732f990
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-09-21 15:57:06 +0000

    Msking bbftp on Mac

--- trunk/platform/Darwin.mk
+++ trunk/platform/Darwin.mk
@@ -5,5 +5,6 @@ export GARLIBEXT=dylib
 export GARSHARED=-dynamic
 export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
 export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
-export MASKED := apps/gcc/gcc apps/base/bash apps/gui/glib apps/gui/Gtk-Perl apps/gui/qt-x11-opensource-src apps/lcg/lcg-gfal apps/lcg/lcg-util apps/lcg/lfc apps/lcg/lfc-perl apps/server/openldap apps/portal/apache
+export MASKED := apps/gcc/gcc apps/base/bash apps/gui/glib apps/gui/Gtk-Perl apps/gui/qt-x11-opensource-src apps/lcg/lcg-gfal apps/lcg/lcg-util apps/lcg/lfc apps/lcg/lfc-perl apps/server/openldap apps/portal/apache apps/base/bbftp
+
 
