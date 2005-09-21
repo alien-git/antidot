05e672a7b81953c0a8e964fbc32c2d0038abe240
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-09-21 15:03:52 +0000

    Masking openldap on Mac - one from the system should be good enough

--- platform/Darwin.mk
+++ platform/Darwin.mk
@@ -5,5 +5,5 @@ export GARLIBEXT=dylib
 export GARSHARED=-dynamic
 export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
 export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
-export MASKED := apps/gcc/gcc apps/base/bash apps/gui/glib apps/gui/Gtk-Perl apps/gui/qt-x11-opensource-src apps/lcg/lcg-gfal apps/lcg/lcg-util apps/lcg/lfc apps/lcg/lfc-perl
+export MASKED := apps/gcc/gcc apps/base/bash apps/gui/glib apps/gui/Gtk-Perl apps/gui/qt-x11-opensource-src apps/lcg/lcg-gfal apps/lcg/lcg-util apps/lcg/lfc apps/lcg/lfc-perl apps/server/openldap
 
