0efa818751e66ebf81d629b97e8045bc1a48fd51
psaiz <psaiz@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-10-03 10:42:16 +0000

    Checking if the system found Config.pm

--- relocate.sh
+++ relocate.sh
@@ -10,8 +10,16 @@ then
    */apps/base/perl)
      echo Relocating $app
      config=`find $prefix/lib/perl5 -name Config.pm -exec grep -l "This file was created by configpm" {} \;`
+     if [ "$config" = "" ] ; then
+	echo "Couldn't find Config.pm in $prefix/lib/perl5!"
+	exit -2
+     fi     
      perl -pi -e "s%$build_prefix/%$prefix/%sg; s%$build_prefix'%$prefix'%sg; s%$build_prefix %$prefix %sg " $config
      config=`find $prefix/lib/perl5 -path "*/CORE/config.h"`
+     if [ "$config" = "" ] ; then
+	echo "Couldn't find CORE/config.h in $prefix/lib/perl5!"
+	exit -2
+     fi
      perl -pi -e "s%$build_prefix/%$prefix/%sg; s%$build_prefix'%$prefix'%sg; s%$build_prefix %$prefix %sg; s%\"$build_prefix\"%\"$prefix\"%sg; " $config
      for file in `find $prefix/lib/perl5 -name .packlist`
      do
