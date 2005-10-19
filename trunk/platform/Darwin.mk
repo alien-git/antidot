8ffafb365f0811d3b16b7d0d22e505c2695b77ba
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2005-10-19 18:39:07 +0000

    Fixing license info and masking gridsite on Darwin

--- trunk/platform/Darwin.mk
+++ trunk/platform/Darwin.mk
@@ -5,6 +5,4 @@ export GARLIBEXT=dylib
 export GARSHARED=-dynamic
 export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
 export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
-export MASKED := apps/gcc/gcc apps/base/bash apps/gui/qt-x11-opensource-src apps/lcg/lcg-gfal apps/lcg/lcg-util apps/portal/apache apps/gui/glib apps/gui/Gtk-Perl apps/gui/Glade-Perl apps/base/bbftp-client apps/base/bbftp-server
-#export MASKED += apps/gui/glib apps/gui/Gtk-Perl apps/lcg/lfc apps/lcg/lfc-perl 
-
+export MASKED := apps/gcc/gcc apps/base/bash apps/gui/qt-x11-opensource-src apps/lcg/lcg-gfal apps/lcg/lcg-util apps/portal/apache apps/gui/glib apps/gui/Gtk-Perl apps/gui/Glade-Perl apps/base/bbftp-client apps/base/bbftp-server apps/portal/gridsite
