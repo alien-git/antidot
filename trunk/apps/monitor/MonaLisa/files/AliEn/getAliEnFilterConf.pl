d94624bd7513ccb0c7c2f5dc541b66cc3dd3b3fd
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2006-03-28 15:39:22 +0000

    Update from Catalin

--- trunk/apps/monitor/MonaLisa/files/AliEn/getAliEnFilterConf.pl
+++ trunk/apps/monitor/MonaLisa/files/AliEn/getAliEnFilterConf.pl
@@ -6,8 +6,12 @@
 # and max_jobs for each defined CE.
 #
 # Catalin Cirstoiu <Catalin.Cirstoiu@cern.ch>
-# Version 0.4 
+# Version 0.6
 #
+# 10/02/2006 - support multiple MLs for a site
+#            - the conf retrieval was rewritten to be more flexible and less hacky
+# 18/01/2006 - added the mapping between CE host names and the corresponding Sites (ML names).
+#              The idea is to be able to identify the site based on the CE host.
 # 13/12/2005 - don't report sites that end in '-L'. Just remove the '-L' and combine the
 #              corresponding domains with the domains from the site without '-L', if exists.
 # 01/12/2005 - if site name's differs from ML name's, use the ML name as the site name.
@@ -44,7 +48,7 @@ if($TESTING){
 my $DEFAULT_APMON_CONFIG = "aliendb5.cern.ch";
 my $verbose = 0;  # print the values on screen.
 
-my ($sites_domains, $ses_hosts, $ces_max_jobs) = getSitesAndDomains();
+my ($sites_domains, $ses_hosts, $ces_max_jobs, $sites_ce_hosts) = getSitesAndDomains();
 
 #sleep(1);
 dumpConf("Sites_domains", $sites_domains);
@@ -52,7 +56,8 @@ dumpConf("Sites_domains", $sites_domains);
 dumpConf("SEs_hosts", $ses_hosts);
 #sleep(1);
 dumpConf("CEs_max_jobs", $ces_max_jobs);
-
+#sleep(1);
+dumpConf("Sites_CE_hosts", $sites_ce_hosts);
 # return 3 hashes:
 # - farm-names -> domains
 # - SE-names -> hosts
@@ -67,33 +72,54 @@ sub getSitesAndDomains {
 	my $lcg_domains = {};
 	my $ses_hosts = {};
 	my $ces_max_jobs = {};
+	my $sites_ce_hosts = {};
 	my $mesg = $ldap->search(base   => "ou=Sites,$ldap_dn",
 				 filter => "objectClass=AliEnSite");
 	my $total = $mesg->count;
 	if(! $total){
 		print "#There are no sites in $ORG\n" if $TESTING;
 	}
-	
+	# Get all defined Sites
 	foreach my $site_entry ($mesg->entries){
-		my $site = $site_entry->get_value("ou") or next;
-		my @domains = $site_entry->get_value("domain");
-		if($site ne "LCG"){ # we don't care about this in case of LCG sites
-#			print "\nFarm: $site\nDomains: @domains\n" if $TESTING;
-			if(! $sites_domains->{$site}){
-				$sites_domains->{$site} = \@domains if(@domains);
-			}else{
-				for my $d (@domains){
-					push(@{$sites_domains->{$site}}, $d) if(! contains($d, @{$sites_domains->{$site}}));
-				}
-			}
+		my $site_name = $site_entry->get_value("ou") or next;
+		my @site_doms = $site_entry->get_value("domain");
+		my $fullLDAPdn = "ou=$site_name,ou=Sites,$ldap_dn";
+
+#		print "SITE $site_name\n";
+		# Get ML services defined for this Site
+		$mesg = $ldap->search(base   => "ou=MonaLisa,ou=Services,$fullLDAPdn",
+				     filter => "objectClass=AliEnMonaLisa");
+		my $ml_count = $mesg->count();
+		my $ml_default; # default ML for the site
+		my $local_mls = {};
+		if($ml_count == 0){
+			$local_mls->{$site_name}->{DOMAINS} = \@site_doms;
+			$ml_default = $site_name;
 		}
-		
-		# Get the SE services - we need SEs hostnames
-		my $fullLDAPdn = "ou=$site,ou=Sites,$ldap_dn";
-		my $msg = $ldap->search(base   => "ou=SE,ou=Services,$fullLDAPdn",
+		foreach my $ml_entry ($mesg->entries){
+			my $ml_name = $ml_entry->get_value("name");
+			my $ml_host = $ml_entry->get_value("host");
+			my @ml_doms = $ml_entry->get_value("domain");
+			push(@ml_doms, @site_doms) if($ml_count == 1); # if site has more MLs, take in account only properties from ML pages
+			$local_mls->{$ml_name}->{DOMAINS} = \@ml_doms;
+			$ml_default = $ml_name if ! defined($ml_default);
+		}
+		# report domains
+		for my $ml_name (keys %{$local_mls}){
+			$sites_domains->{$ml_name} = [] if ! defined($sites_domains->{$ml_name});
+			for my $dom (@{$local_mls->{$ml_name}->{DOMAINS}}){
+				push(@{$sites_domains->{$ml_name}}, $dom) if ! contains($dom, @{$sites_domains->{$ml_name}});
+			}
+#			print "DOM $ml_name -> @{$sites_domains->{$ml_name}}\n";
+		}
+#		print "ML_default -> $ml_default\n";
+
+		# Get the SEs defined in this site - we need their hostnames
+		$mesg = $ldap->search(base   => "ou=SE,ou=Services,$fullLDAPdn",
 					filter => "(|(objectClass=AliEnMSS)(objectClass=AliEnSE))");
-		foreach my $se_entry ($msg->entries){
-			my $se_name = ucfirst($ORG)."::".$site."::".$se_entry->get_value("name");
+		foreach my $se_entry ($mesg->entries){
+			my $se_name = $se_entry->get_value("name");
+			my $se_fullName = ucfirst($ORG)."::".$site_name."::".$se_name;
 			my @se_hosts = ();
 			my $se_host = $se_entry->get_value("host");
 			push(@se_hosts, $se_host) if $se_host;
@@ -105,47 +131,107 @@ sub getSitesAndDomains {
 				}
 			}
 #			print "SE_Name: $se_name\nSE_Hosts: @se_hosts\n" if $TESTING;
-			$ses_hosts->{$se_name} = \@se_hosts;
+			$ses_hosts->{$se_fullName} = \@se_hosts;  # report directly
 		}
-
-		# Get the CE services - we need the maximum number of jobs for each CE
-		$msg = $ldap->search(base   => "ou=CE,ou=Services,$fullLDAPdn",
-				     filter => "objectClass=AliEnCE");
-		foreach my $ce_entry ($msg->entries){
-			my $ce_name = ucfirst($ORG)."::".$site."::".$ce_entry->get_value("name");
+		
+#		print "QCE ==> ou=CE,ou=Services,$fullLDAPdn\n";
+		# Get the CEs defined in this site - we need maxjobs and hostnames
+		$mesg = $ldap->search(base   => "ou=CE,ou=Services,$fullLDAPdn",
+				      filter => "objectClass=AliEnCE");
+		my $ce_default; # default CE for the site
+		my $local_ces = {};
+		foreach my $ce_entry ($mesg->entries){
+			my $ce_name = $ce_entry->get_value("name");
+			my $ce_fullName = ucfirst($ORG)."::".$site_name."::".$ce_name;
 			my $max_jobs = $ce_entry->get_value("maxjobs");
-			$ces_max_jobs->{$ce_name} = [$max_jobs];
-#			print "CE_Name: $ce_name; max_jobs: $max_jobs\n";
+			my @ce_hosts = $ce_entry->get_value("host");
+			$local_ces->{$ce_name}->{HOSTS} = \@ce_hosts;
+#			print "CE $ce_fullName -> @ce_hosts\n";
+			$ce_default = $ce_name if ! defined($ce_default);
+			$ces_max_jobs->{$ce_fullName} = [$max_jobs]; # report directly
 		}
-
-		# Get ML services - mainly for LCG site domains, but if normal sites
-		# have a domain specified in ML ldap properties, that will be taken also.
-		$msg = $ldap->search(base   => "ou=MonaLisa,ou=Services,$fullLDAPdn",
-				     filter => "objectClass=AliEnMonaLisa");
-		foreach my $ml_entry ($msg->entries){
-			my $ml_name = $ml_entry->get_value("name");
-			$ml_name = $1 if $ml_name =~ /(.+)-L/;
-			my @ml_domains = $ml_entry->get_value("domain");
-			# rename the site name to the ML name, if site name isn't LCG
-			if($site ne $ml_name && $site ne "LCG"){
-				$sites_domains->{$ml_name} = $sites_domains->{$site};
-				delete $sites_domains->{$site};
-			}
-			if(! $sites_domains->{$ml_name}){
-				$sites_domains->{$ml_name} = \@ml_domains if(@ml_domains);
-			}else{
-				for my $d (@ml_domains){
-					push(@{$sites_domains->{$ml_name}}, $d) if(! contains($d, @{$sites_domains->{$ml_name}}));
+		
+#		print "CE_default -> ".($ce_default || "")."\n";
+		
+		# Get Config for different hosts in this site, to discover associations between MLs and CEs, etc.
+		$mesg = $ldap->search(base   => "ou=Config,$fullLDAPdn",
+				     filter => "objectClass=AliEnHostConfig");
+		my $conf_count = $mesg->count();
+		if($conf_count == 0){
+			$sites_ce_hosts->{$ml_default} = [] if ! $sites_ce_hosts->{$ml_default};
+			for my $ce_n (keys %{$local_ces}){
+				for my $ce_h (@{$local_ces->{$ce_n}->{HOSTS}}){
+					push(@{$sites_ce_hosts->{$ml_default}}, $ce_h) if ! contains($ce_h, @{$sites_ce_hosts->{$ml_default}});
 				}
 			}
 		}
-		if($TESTING){
-			$ces_max_jobs->{"alienbuild::CERN::testCE"} = [6];
-			$ces_max_jobs->{"oplapro12::CERN::testCE"} = [6];
+		# make the correlations CE <-> ML
+		foreach my $conf_entry ($mesg->entries){
+			my $ce_name = $conf_entry->get_value("ce") || $ce_default;
+			my $ml_name = $conf_entry->get_value("monalisa") || $ml_default;
+			next if (! $ce_name) || (! $local_ces->{$ce_name});
+			$sites_ce_hosts->{$ml_name} = [] if ! $sites_ce_hosts->{$ml_name};
+#			print "CONNECT $ml_name -> @{$local_ces->{$ce_name}->{HOSTS}}\n";
+			for my $ce_h (@{$local_ces->{$ce_name}->{HOSTS}}){
+				push(@{$sites_ce_hosts->{$ml_name}}, $ce_h) if ! contains($ce_h, @{$sites_ce_hosts->{$ml_name}});
+			}
+#			print "CEs $ml_name -> @{$sites_ce_hosts->{$ml_name}}\n";
 		}
 	}
+	
+	if($TESTING){
+		$ces_max_jobs->{"pcardaab::CERN::testCE"} = [6];
+		$ces_max_jobs->{"alienbuild::CERN::testCE"} = [6];
+		$ces_max_jobs->{"oplapro12::CERN::testCE"} = [6];
+		$ces_max_jobs->{"alimacx08::CERN::testCE"} = [6];
+		$ces_max_jobs->{"pcaliceamd01::CERN::testCE"} = [6];
+		$ces_max_jobs->{"lxb2075::CERN::testCE"} = [6];
+
+		$sites_ce_hosts->{"AliEn2-TEST-pcardaab"} = ['pcardaab.cern.ch'];
+		$sites_ce_hosts->{"AliEn2-TEST-alienbuild"} = ['alienbuild.cern.ch'];
+		$sites_ce_hosts->{"AliEn2-TEST-oplapro12"} = ['oplapro12.cern.ch'];
+		$sites_ce_hosts->{"AliEn2-TEST-alimacx08"} = ['alimacx08.cern.ch'];
+		$sites_ce_hosts->{"AliEn2-TEST-pcaliceamd01"} = ['pcaliceamd01.cern.ch'];
+		$sites_ce_hosts->{"AliEn2-TEST-lxb2075"} = ['lxb2075.cern.ch'];
+	}
 	$mesg = $ldap->unbind;
-	return ($sites_domains, $ses_hosts, $ces_max_jobs);
+	
+	filterDomains($sites_domains);
+
+	return ($sites_domains, $ses_hosts, $ces_max_jobs, $sites_ce_hosts);
+}
+
+# remove similar domains from different sites
+# keep the domain only in the first encountered site
+sub filterDomains {
+	my $sites_domains = shift;
+	
+	my @sites = sort(keys %$sites_domains);
+	for(my $i=0; $i<@sites; $i++){
+		my $site1 = $sites[$i];
+		my $domains1 = $sites_domains->{$site1};
+		for(my $j=$i+1; $j<@sites; $j++){
+			my $site2 = $sites[$j];
+			my $domains2 = $sites_domains->{$site2};
+			for(my $k=0; $k<@$domains2; $k++){
+				my $dom2 = $$domains2[$k];
+				if($dom2 !~ /^>/ && contains($dom2, @$domains1)){
+#					print "Reporting for $dom2 in $site2 as $site1\n";
+					splice(@$domains2, $k, 1, (">$site1"));
+					$k--;
+				}
+			}
+		}
+	}
+}
+
+# add $what to the given list and return the reference to the result.
+sub addIfMissing {
+	my $what = shift;
+	my @where = @_ || ();
+
+	push(@where, $what) if(! contains($what, @where));
+	return \@where;
 }
 
 # check if the first given parameter is among all the others
@@ -189,11 +275,11 @@ sub getLDAP_DN {
 	my $ldap = Net::LDAP->new('alien.cern.ch:8389') or die "$@";
 	$ldap->bind;    # an anonymous bind
 	my $mesg = $ldap->search(base   => "o=alien,dc=cern,dc=ch",
-				 filter => "(ou=$ENV{ALIEN_ORGANISATION})"
+				 filter => "(ou=$ORG)"
 				);
 	$mesg->code && die $mesg->error;
 	if(! $mesg->count){
-  		print "#ERROR: There is no organisation called '$ENV{ALIEN_ORGANISATION}'\n";
+  		print "#ERROR: There is no organisation called '$ORG'\n";
 		return "";
 	}
 	my $entry    = $mesg->entry(0);
