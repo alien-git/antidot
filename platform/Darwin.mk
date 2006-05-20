20241107a574aa6360851a6b720fb79acb433ee9
buncic <buncic@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2006-05-20 06:49:06 +0000

    Masking loadmouth on Darwin

--- platform/Darwin.mk
+++ platform/Darwin.mk
@@ -5,4 +5,4 @@ export GARLIBEXT=dylib
 export GARSHARED=-dynamic
 export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
 export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
-export MASKED := apps/gcc/gcc apps/base/bash apps/gui/qt-x11-opensource-src apps/lcg/lcg-gfal apps/lcg/lcg-util apps/portal/apache apps/gui/glib apps/gui/Gtk-Perl apps/gui/Glade-Perl apps/portal/gridsite
+export MASKED := apps/gcc/gcc apps/base/bash apps/lcg/lcg-gfal apps/lcg/lcg-util apps/portal/apache apps/gui/glib apps/gui/Gtk-Perl apps/gui/Glade-Perl apps/portal/gridsite apps/base/loadmouth apps/base/libgcrypt apps/base/libgpg-error apps/base/gnutls
