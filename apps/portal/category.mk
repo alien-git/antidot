63d913556ba562374106042a62cae33d524f8cae
nobody <nobody@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2006-06-21 13:54:26 +0000

    This commit was manufactured by cvs2svn to create tag 'blabla'.

--- apps/portal/category.mk
+++
@@ -1,8 +0,0 @@
-# extra configure info
-# CONFIGURE_ARGS += --disable-debug
-
-APACHE=1.3.33
-
-export GARDIR := ../../../
-
-include ../../../gar.mk
--- platform/Darwin.mk
+++
@@ -1,8 +0,0 @@
-TARGET_LIBNAME=lib
-export MACOSX_DEPLOYMENT_TARGET=10.4
-unexport LD_PREBIND
-export GARLIBEXT=dylib
-export GARSHARED=-dynamic
-export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
-export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
-export MASKED := apps/gcc/gcc apps/base/bash apps/lcg/lcg-gfal apps/lcg/lcg-util apps/portal/apache apps/gui/glib apps/gui/Gtk-Perl apps/gui/Glade-Perl apps/portal/gridsite apps/base/loudmouth apps/base/libgcrypt apps/base/libgpg-error apps/base/gnutls
