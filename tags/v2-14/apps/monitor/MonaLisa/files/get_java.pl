4d42fb5af5b4007298d112cdea5514d67292cf48
nobody <nobody@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2008-04-04 16:18:23 +0000

    This commit was manufactured by cvs2svn to create tag 'v2-14'.

---
+++ tags/v2-14/apps/monitor/MonaLisa/files/get_java.pl
@@ -0,0 +1,47 @@
+#!/usr/local/bin/perl
+# This will attempt to download the 1.5.0 java virtual machine for a given
+# platform on linux.
+
+# If it's ia64, it will get a tar.bz2 file (repackaged from the jrockit JVM), otherwise
+# it will take the default sun JVM.
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+
+my $platform = shift;
+my $java_ver = shift;
+my @url_base = ();
+
+while(my $u = shift){
+	push(@url_base, $u);
+}
+
+die "Usage:\n\tget_java.pl <platform> <java_version> <url_base1> [ <url_base2> ... ]\n" 
+	unless $java_ver && @url_base && $platform;
+
+my $ua = LWP::UserAgent->new;
+$ua->agent("get_java.pl/1.0 ");
+$ua->env_proxy();
+
+my $success = 0;
+my $file_name = "jdk-$java_ver-$platform.".($platform =~ /ia64/ ? "tar.bz2" : "bin");
+my $res;
+for my $base (@url_base){
+	my $url = "${base}../java/${file_name}";
+	
+	my $req = HTTP::Request->new(GET => $url);
+	print "Getting $url ... ";
+	$res = $ua->request($req, $file_name);
+	if(! $res->is_error()){
+		$success = 1;
+		print "ok!\n";
+		last;
+	}
+	print "failed.\n";
+}
+
+die "Failed to download '$file_name'\n".$res->status_line() if ! $success;
+
+print "JVM binary successfully retrieved!\n$file_name\n";
+
