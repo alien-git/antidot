eb73681b9e7c21999a2cb70e1a8c34a72f2e85f4
alina <alina@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2009-01-13 13:11:20 +0000

    - taking out UseGCOverheadLimit from jvm options

--- trunk/apps/monitor/MonaLisa/files/AliEn/startMonaLisa.pl
+++ trunk/apps/monitor/MonaLisa/files/AliEn/startMonaLisa.pl
@@ -255,7 +255,7 @@ sub setupConfig {
 	die("MonaLisa hostname from LDAP config [".$config->{MONALISA_HOST}."] differs from local one [$fqdn]. Not starting it...\n");
     }
     my $shouldUpdate = ($config->{MONALISA_SHOULDUPDATE} or "true");
-    my $javaOpts = ($config->{MONALISA_JAVAOPTS} or "-Xms96m -Xmx96m -XX:-UseGCOverheadLimit");
+    my $javaOpts = ($config->{MONALISA_JAVAOPTS} or "-Xms96m -Xmx96m");
     my $add = [];
     my $rmv = [];
     my $changes = {
