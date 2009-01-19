1d1c41103666da345d1ba71494a740fd0ec4c36f
alina <alina@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2009-01-19 08:48:47 +0000

    removing inexinting packages

--- platform/Darwin.mk
+++ platform/Darwin.mk
@@ -5,5 +5,5 @@ export GARLIBEXT=dylib
 export GARSHARED=-dynamic
 export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
 export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
-export MASKED := apps/gcc/gcc= apps/base/bash= apps/lcg/lcg-dm= apps/lcg/lcg-gfal= apps/lcg/lcg-util= apps/portal/apache= apps/gui/glib= apps/gui/Gtk-Perl= apps/gui/Glade-Perl= apps/portal/gridsite= apps/base/loudmouth= apps/base/libgcrypt= apps/base/libgpg-error= apps/base/gnutls= apps/base/opencdk= apps/base/cctools= apps/base/OpenCA= apps/server/ejabberd= apps/server/otp= apps/base/xrootd-tokenauthzofs= apps/portal/httpd= apps/portal/gridsite= apps/portal/mod_macro= apps/portal/mod_perl=
+export MASKED := apps/gcc/gcc= apps/base/cctools= apps/base/OpenCA= apps/base/xrootd-tokenauthzofs= apps/portal/httpd= apps/portal/gridsite= apps/portal/mod_macro= apps/portal/mod_perl=
 
