53e886cf4fb244c42e1882bf9b56eb309ac6d353
nobody <nobody@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2010-01-24 09:32:47 +0000

    This commit was manufactured by cvs2svn to create tag 'v2-17'.

--- platform/Darwin.mk
+++ platform/Darwin.mk
@@ -5,5 +5,5 @@ export GARLIBEXT=dylib
 export GARSHARED=-dynamic
 export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
 export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
-export MASKED := apps/gcc/gcc= apps/base/cctools= apps/base/OpenCA= apps/base/xrootd-tokenauthzofs= apps/portal/httpd= apps/portal/gridsite= apps/portal/mod_macro= apps/portal/mod_perl= apps/base/kerberos= apps/base/aria2= apps/devel/autoconf= apps/devel/automake= apps/devel/libtool= apps/devel/jdk=
+export MASKED := apps/gcc/gcc= apps/base/cctools= apps/base/OpenCA= apps/base/xrootd-tokenauthzofs= apps/portal/httpd= apps/portal/gridsite= apps/portal/mod_macro= apps/portal/mod_perl= apps/base/kerberos= apps/base/aria2= apps/devel/autoconf= apps/devel/automake= apps/devel/libtool=
 
