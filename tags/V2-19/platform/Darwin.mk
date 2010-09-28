040fe3d0bb0743347350ee095fa6335d54b850ef
nobody <nobody@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2010-09-28 13:41:58 +0000

    This commit was manufactured by cvs2svn to create tag 'V2-19'.

---
+++ tags/V2-19/platform/Darwin.mk
@@ -0,0 +1,9 @@
+TARGET_LIBNAME=lib
+export MACOSX_DEPLOYMENT_TARGET=10.5
+unexport LD_PREBIND
+export GARLIBEXT=dylib
+export GARSHARED=-dynamic
+export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
+export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
+export MASKED := apps/gcc/gcc= apps/base/cctools= apps/base/OpenCA= apps/base/xrootd-tokenauthzofs= apps/portal/httpd= apps/portal/gridsite= apps/portal/mod_macro= apps/portal/mod_perl= apps/base/kerberos= apps/base/aria2= apps/devel/autoconf= apps/devel/automake= apps/devel/libtool= apps/devel/jdk= apps/base/google-perftools=
+
