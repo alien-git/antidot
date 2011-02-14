504899bbcbaeaad6057adc278ee8cabf75e4e7b4
alina <alina@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2011-02-14 16:45:46 +0000

    adding ALIEN_DOMAIN and mime.types

--- apps/portal/httpd/files/startup.pl
+++ apps/portal/httpd/files/startup.pl
@@ -6,49 +6,15 @@ my @services=qw( PackMan );
 
 my $userID = getpwuid($<);
 
-my $tmpEnv = `grep -m 1 ALIEN_HOME $ENV{HOME}/.alien/Environment`;
-chomp $tmpEnv;
-(undef, $tmpEnv) = split (/=\s*/, $tmpEnv);
-$ENV{ALIEN_HOME}= ( $tmpEnv || $ENV{ALIEN_HOME} || "/home/$userID/.alien" );
 
-$tmpEnv = undef;
-$tmpEnv = `grep -m 1 ALIEN_ROOT $ENV{HOME}/.alien/Environment`;
-chomp $tmpEnv;
-(undef, $tmpEnv) = split (/=\s*/, $tmpEnv);
-$ENV{ALIEN_ROOT}= ( $tmpEnv || $ENV{ALIEN_ROOT} || "/home/$userID/alien") ; 
-
-
-$tmpEnv = undef;
-$tmpEnv = `grep -m 1 ALIEN_USER $ENV{HOME}/.alien/Environment`;
-chomp $tmpEnv;
-(undef, $tmpEnv) = split (/=\s*/, $tmpEnv);
-$ENV{ALIEN_USER} = ( $tmpEnv || $ENV{ALIEN_USER} || "$userID" );
-
-$tmpEnv = undef;
-$tmpEnv = `grep -m 1 ALIEN_ORGANISATION $ENV{HOME}/.alien/Environment`;
-chomp $tmpEnv;
-(undef, $tmpEnv) = split (/=\s*/, $tmpEnv);
-$ENV{ALIEN_ORGANISATION}= ( $tmpEnv || $ENV{ALIEN_ORGANISATION} ||  "ALICE" );
-
-$tmpEnv = undef;
-$tmpEnv = `grep -m 1 GLOBUS_LOCATION $ENV{HOME}/.alien/Environment`;
-chomp $tmpEnv;
-(undef, $tmpEnv) = split (/=\s*/, $tmpEnv);
-$ENV{GLOBUS_LOCATION}= ( $tmpEnv || $ENV{GLOBUS_LOCATION} || "$ENV{ALIEN_ROOT}/globus" ) ;
-
-$tmpEnv = undef;
-$tmpEnv = `grep -m 1 X509_USER_PROXY $ENV{HOME}/.alien/Environment`;
-chomp $tmpEnv;
-(undef, $tmpEnv) = split (/=\s*/, $tmpEnv);
-$ENV{X509_USER_PROXY}=( $tmpEnv || $ENV{X509_USER_PROXY} || "/tmp/x509up_u$<" );
-
-$tmpEnv = undef;
-$tmpEnv = `grep -m 1 X509_CERT_DIR $ENV{HOME}/.alien/Environment`;
-chomp $tmpEnv;
-(undef, $tmpEnv) = split (/=\s*/, $tmpEnv);
-$ENV{X509_CERT_DIR}=( $tmpEnv || $ENV{X509_CERT_DIR} || "$ENV{GLOBUS_LOCATION}/share/certificates" );
-
-#$ENV{ALIEN_LDAP_DN}="alice-ldap.cern.ch:8389/o=alice,dc=cern,dc=ch"; 
+$ENV{ALIEN_HOME} = ( $ENV{ALIEN_HOME} || "/home/$userID/.alien" );
+$ENV{ALIEN_ROOT} = ( $ENV{ALIEN_ROOT} || "/home/$userID/alien") ; 
+$ENV{ALIEN_USER} = ( $ENV{ALIEN_USER} || "$userID" );
+$ENV{ALIEN_ORGANISATION} = ($ENV{ALIEN_ORGANISATION} ||  "ALICE" );
+$ENV{GLOBUS_LOCATION} = ( $ENV{GLOBUS_LOCATION} || "$ENV{ALIEN_ROOT}/globus" ) ;
+$ENV{X509_USER_PROXY} = ( $ENV{X509_USER_PROXY} || "/tmp/x509up_u$<" );
+$ENV{X509_CERT_DIR} = ( $ENV{X509_CERT_DIR} || "$ENV{GLOBUS_LOCATION}/share/certificates" );
+#$ENV{ALIEN_LDAP_DN} = "alice-ldap.cern.ch:8389/o=alice,dc=cern,dc=ch"; 
 
 my $l=AliEn::Logger->new();
 $l->infoToSTDERR();
