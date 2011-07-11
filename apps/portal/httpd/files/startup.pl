314be7a3325d3722e02da0d72c1f82b46afa14e1
psaiz <psaiz@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2011-07-11 13:59:30 +0000

    Adding a message to the log

--- apps/portal/httpd/files/startup.pl
+++ apps/portal/httpd/files/startup.pl
@@ -30,6 +30,7 @@ foreach my $s (@services) {
     eval "require $name" or die("Error requiring the module: $@");
     my $serv=$name->new() ;
     $serv or exit(-2);
+    $l->info("Starting $s on httpd");
   };
   if ($@) {
     print "NOPE!!\n $@\n";
