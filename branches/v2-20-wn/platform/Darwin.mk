7f1562843a9ed8166d9c61257e2fa87c7bebc402
alinag <alinag@5de04bae-c131-43cd-a2a6-5bb0fd9e62ce>
2012-09-05 14:57:03 +0000

    branching for alien wn

---
+++ branches/v2-20-wn/platform/Darwin.mk
@@ -0,0 +1,16 @@
+TARGET_LIBNAME=lib
+export MACOSX_DEPLOYMENT_TARGET=10.6
+unexport LD_PREBIND
+export GARLIBEXT=dylib
+export GARSHARED=-dynamic
+export OWN_LDFLAGS += -dylib -undefined dynamic_lookup -single_module
+export OWN_CXXLDFLAGS += -dynamiclib -undefined dynamic_lookup -single_module
+export MASKED := \
+apps/gcc/gcc= \
+apps/system/kerberos= \
+apps/devel/autoconf= \
+apps/devel/automake= \
+apps/portal/gridsite= \
+apps/devel/libtool= \
+apps/devel/jdk= \
+apps/system/openssl= \
